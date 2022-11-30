961c97ec7345c07acbcf8ea4db64f33ebc45e066
UsagihimeDenka <philippe8910@gmail.com>
2022-11-30 16:39:52 +0800

    added Stone sound Effect

---
+++ Assets/Scripts/State/PullRodState.cs
@@ -0,0 +1,21 @@
+﻿namespace State
+{
+    public class PullRodState : IState
+
+    {
+        public void OnEnterState(object action)
+        {
+            
+        }
+
+        public void OnStayState(object action)
+        {
+            
+        }
+
+        public void OnExitState(object action)
+        {
+            
+        }
+    }
+}
