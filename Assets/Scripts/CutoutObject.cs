6f79ca78a8af16df471c2308060aed9335662ef7
UsagihimeDenka <philippe8910@gmail.com>
2022-12-11 16:23:05 +0800

    import owo

--- Assets/Scripts/CutoutObject.cs
+++ Assets/Scripts/CutoutObject.cs
@@ -1,8 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CutoutObject : MonoBehaviour
 {
-   
+    private void Start()
+    {
+        
+    }
+
+    public int Plus(int a , int b)
+    {
+        
+        return a + b;
+    }
+    
+    public int SmallestEvenMultiple(int n)
+    {
+        int ans = 0;
+        int take = 1;
+        while (true)
+        {
+            if ((n * take) % 2 == 0)
+            {
+                break;
+            }
+            take++;
+            ans = n * take;
+            
+        }
+        return ans;
+    }
 }
