b50760446db4922e16ffcd09a8784dd8ea140e2f
UsagihimeDenka <philippe8910@gmail.com>
2022-12-11 18:10:33 +0800

    added Fire

---
+++ Assets/EventTaskInvoker.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventTaskInvoker : MonoBehaviour
+{
+    [SerializeField] private List<UnityEvent> data = new List<UnityEvent>();
+}
+
