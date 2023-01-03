40e67ab47a0d2d62f1a621fd4873a966f82f6f30
PhilippeCheng <philippe8910@gmail.com>
2023-01-04 02:11:08 +0800

    Fixed Float cube

--- Assets/FloatCube.cs
+++ Assets/FloatCube.cs
@@ -1,23 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class FloatCube : MonoBehaviour
 {
     private Vector3 defaultCenter;
 
-    private Rigidbody rigidbody;
+    [SerializeField] private float speed;
     // Start is called before the first frame update
     void Start()
     {
         defaultCenter = transform.position;
-
-        TryGetComponent(out rigidbody);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position , defaultCenter , 0.02f);
+        transform.position = Vector3.Lerp(transform.position , defaultCenter , Time.deltaTime * speed);
     }
+    
+    
 }
