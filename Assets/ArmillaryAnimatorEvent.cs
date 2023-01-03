057b66efda513f08fc6e71ee07e365eff83af9b2
PhilippeCheng <philippe8910@gmail.com>
2023-01-03 21:45:41 +0800

    Fixed Armillary animator event

--- Assets/ArmillaryAnimatorEvent.cs
+++ Assets/ArmillaryAnimatorEvent.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,31 @@ using UnityEngine;
 public class ArmillaryAnimatorEvent : MonoBehaviour
 {
     public GameObject armillaryGameObject;
+    
 
-    public void OnAnimationEnd()
+    private void Start()
     {
-        armillaryGameObject.SetActive(true);
-        gameObject.SetActive(false);
+        TryGetComponent(out Animator animator);
+
+        StartCoroutine(StartDetected());
+
+        IEnumerator StartDetected()
+        {
+            AnimatorStateInfo info = animator.GetCurrentAnimatorStateInfo(0);
+            
+            if (info.normalizedTime < 1)
+            {
+                yield return new WaitForSeconds(.1f);
+                Debug.Log(info.normalizedTime);
+                StartCoroutine(StartDetected());
+            }
+            if(info.normalizedTime >= 1)
+            {
+                armillaryGameObject.SetActive(true);
+                gameObject.SetActive(false);
+                Debug.Log("Detecting!!!");
+                yield return null;
+            }
+        }
     }
 }
