c86e860980c83313c7c4bf17b1fea4f2ce68bff8
PhilippeCheng <philippe8910@gmail.com>
2023-01-06 11:44:01 +0800

    added bossbullet

---
+++ Assets/BossBullet.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBullet : MonoBehaviour
+{
+    public Vector3 targetPosition;
+    
+    public Vector3 targetVector3;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = FindObjectOfType<AamonAction>().transform.position;
+        transform.LookAt(targetPosition);
+
+        targetVector3 = transform.position - targetPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= targetVector3 * Time.deltaTime * 2;
+    }
+}
---
+++ Assets/Scripts/Event/ChangeBossLevelState.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection.Emit;
+
+namespace Event
+{
+    public class ChangeBossLevelState
+    {
+        
+    }
+}
