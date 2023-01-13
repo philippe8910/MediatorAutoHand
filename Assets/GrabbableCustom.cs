02cf5f55a91b817dfb1e2d2f12083713e1076e90
UsagihimeDenka <philippe8910@gmail.com>
2023-01-13 22:58:26 +0800

    added ghost effect and Fixed Double Player bug

--- Assets/GrabbableCustom.cs
+++ Assets/GrabbableCustom.cs
@@ -5,10 +5,15 @@ using Autohand;
 using Event;
 using Project;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 [RequireComponent(typeof(Rigidbody) , typeof(Grabbable) , typeof(DistanceGrabbable))]
 public class GrabbableCustom : MonoBehaviour
 {
+    [SerializeField] private GameObject cube;
+    
+    private bool isGrab;
+    
     private Grabbable _grabbable;
 
     public List<Transform> robotPoint = new List<Transform>();
@@ -16,6 +21,7 @@ public class GrabbableCustom : MonoBehaviour
     void Start()
     {
         TryGetComponent<Grabbable>(out _grabbable);
+        TryGetComponent<DistanceGrabbable>(out var _distanceGrabbable);
         
         _grabbable.OnHighlightEvent += delegate(Hand hand, Grabbable grabbable)
         {
@@ -26,11 +32,59 @@ public class GrabbableCustom : MonoBehaviour
         {
             EventBus.Post(new PlayerSelectGrabbableObjectDetected(null)); 
         };
+        
+        _grabbable.onGrab.AddListener(delegate(Hand arg0, Grabbable grabbable)
+        {
+            isGrab = true;
+        });
+        
+        _distanceGrabbable.OnPull.AddListener(delegate(Hand arg0, Grabbable grabbable)
+        {
+            OnStartPull();
+        });
     }
 
     public Transform GetIndexVector(int index)
     {
         return robotPoint[index];
     }
+
+    public void OnStartPull()
+    {
+        isGrab = false;
+
+        TryGetComponent(out Rigidbody rig);
+        
+        rig.AddTorque(new Vector3(0.1f , 0.1f, 0.1f));
+        
+        StartCoroutine(StartPull());
+        
+        IEnumerator StartPull()
+        {
+            if (!isGrab)
+            {
+                var g = Instantiate(cube, transform.position, transform.rotation);
+                 
+                
+                Destroy(g , 0.55f);
+                yield return new WaitForSeconds(0.01f);
+                StartCoroutine(StartPull());
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+    }
     
 }
+
+/*
+                var material = g.GetComponent<MeshRenderer>().material;
+
+                Material newMaterial = material;
+
+                newMaterial.color = new Color(newMaterial.color.r + 0.01f, newMaterial.color.g,
+                    newMaterial.color.b + 0.01f);
+                g.GetComponent<MeshRenderer>().material = newMaterial;
+*/
