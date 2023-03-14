00ef46ab0c2890a5b30cf46c4873be9a2e63a0e8
Tosto <j27272589@gmail.com>
2023-03-14 15:18:59 +0800

    2-5 scene water & tornado update

---
+++ Assets/water/GabrielAguiarProductions/Scripts/ExplodeScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeScript : MonoBehaviour {
+    
+	public float minForce;
+	public float maxForce;
+	public float radius;
+	public float rescaleDelay;
+	public float rescaleAmount;
+	public float rescaleMultiplier;
+
+	private WaitForSeconds _smallDelay = new WaitForSeconds (0.01f);
+
+	public void Explode () {
+		foreach (Transform t in transform){
+			var rb = t.GetComponent<Rigidbody> ();
+
+			if (rb != null)
+            {
+				rb.AddExplosionForce (Random.Range(minForce, maxForce), transform.position, radius);
+			}
+		}
+	}
+}
