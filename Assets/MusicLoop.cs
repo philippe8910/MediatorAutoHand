93445268742b2d3aecbe22e5b8a9463837a76ec7
nibby <nibby4660@gmail.com>
2023-03-17 21:39:25 +0800

    add music loop

---
+++ Assets/MusicLoop.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicLoop : MonoBehaviour
+{
+    public AudioSource musicSource;
+    public AudioClip musicStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicSource.PlayOneShot(musicStart);
+        musicSource.PlayScheduled(AudioSettings.dspTime + musicStart.length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
